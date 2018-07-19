01fc7c90ceb77d86d3cae5d493f3ce8823e61825
Golden7ty8 <33757527+Golden7ty8@users.noreply.github.com>
2018-07-19 10:04:45 -0300

    Initial commit

---
+++ Assets/Scripts/BuildVersion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* 
+ * Simply stores a value representing the build version
+ * for organizational perposes.
+ * 
+ * Created by GoldenWay on 7/18/2018
+ * Modified by GoldenWay on 7/18/2018
+ * 
+ */
+
+public class BuildVersion : MonoBehaviour {
+
+	public string buildVersion;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
