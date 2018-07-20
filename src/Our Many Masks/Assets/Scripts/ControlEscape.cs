ef4eef5f7980d4667615239fbb21b898283c59cf
Golden7ty8 <33757527+Golden7ty8@users.noreply.github.com>
2018-07-20 09:21:42 -0300

    0.0.2* Changes
    Escape key functionality updated.

---
+++ src/Our Many Masks/Assets/Scripts/ControlEscape.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Manages the escape control, which should allow you to
+ * exit to a previous menu or if you are on the main menu, will quit.
+ * 
+ * Created by GoldenWay on 7/20/2018
+ * Modified by GoldenWay on 7/20/2018
+ * 
+ */
+
+public class ControlEscape : MonoBehaviour {
+
+	public GameObject mainMenu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Escape pressed?
+		if (Input.GetAxisRaw ("Cancel") == 1) {
+			//Are we on the main menu or the lock screen?
+			if (QuickLinks.quickLinks.uIManager.currentMenu == mainMenu || QuickLinks.quickLinks.uIManager.currentMenu.name == "Lock Screen") {
+				Application.Quit ();
+			} else {
+				QuickLinks.quickLinks.uIManager.ActivateMenu (mainMenu);
+			}
+		}
+	}
+}
---
+++ src/Our Many Masks/Assets/Scripts/QuickLinks.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Provides easy links to objects and scripts in other scripts
+ * by simply type QuickLinks.quickLinks.* where * is any public
+ * non-static value listed below.
+ * 
+ * Created by GoldenWay on 7/20/2018
+ * Modified by GoldenWay on 7/20/2018
+ * 
+ */
+
+public class QuickLinks : MonoBehaviour {
+
+	public static QuickLinks quickLinks;
+
+	[Header("References:")]
+	public UIManager uIManager;
+
+	void Awake () {
+		quickLinks = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
