ddd5f636b74d65a3871db55855389970c1bf9319
Golden7ty8 <33757527+Golden7ty8@users.noreply.github.com>
2018-07-21 20:47:59 -0300

    First Debug Display Changes

---
+++ src/Our Many Masks/Assets/Scripts/ControlDebug.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Manages Debug controls.
+ * 
+ * Created by GoldenWay on 7/21/2018
+ * Modified by GoldenWay on 7/21/2018
+ * 
+ */
+
+public class ControlDebug : MonoBehaviour {
+
+	public GameObject debugDisplay;
+
+	bool togglePressed = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DebugControlsManager ();
+	}
+
+	void DebugControlsManager () {
+		if (Debug.isDebugBuild) {
+			if (Input.GetAxisRaw ("Debug Toggle") == 1) {
+				if (!togglePressed) {
+					togglePressed = true;
+					debugDisplay.SetActive (!debugDisplay.activeSelf);
+				}
+			} else {
+				togglePressed = false;
+			}
+		}
+	}
+}
