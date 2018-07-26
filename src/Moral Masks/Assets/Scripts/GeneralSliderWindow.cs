90ac49cec3cd41d0aec95ab4f5a670422b3d6660
Golden7ty8 <33757527+Golden7ty8@users.noreply.github.com>
2018-07-26 10:57:35 -0300

    GeneralSliderWindow Globbalizing code
    Making the script more widly usable for any sliding windows.

--- src/Moral Masks/Assets/Scripts/GeneralSliderWindow.cs
+++ src/Moral Masks/Assets/Scripts/GeneralSliderWindow.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*
- * Manages the toolbar functionality.
+ * Controls sliding window functionality.
  * 
  * Created by GoldenWay on 7/21/2018
- * Modified by GoldenWay on 7/24/2018
+ * Modified by GoldenWay on 7/26/2018
  * 
  */
 
 public class GeneralSliderWindow : MonoBehaviour {
 
-	[Header("Options:")] [Range(0.0f, 1.0f)] [Tooltip("How visible is the toolbar when closed? (1 being compleatly hidden and 0 being covering the whole screen).")]
+	public enum ScreenEdge {Left, Right, Up, Down};
+
+	[Header("Options:")] [Range(0.0f, 1.0f)] [Tooltip("How visible is the window when closed? (1 being compleatly hidden and 0 being covering the whole screen).")]
 	public float closedPercentage;
 	public float openPercentage;
 	public float slideSpeed;
-	[Tooltip("False for vertical sliding, true for horizontal sliding.")]
-	public bool horizontal;
+	//[Tooltip("False for vertical sliding, true for horizontal sliding.")]
+	//public bool horizontal;
+	public ScreenEdge edge;
 
 	[Header("References:")]
 	public GameObject panel;
@@ -37,18 +40,34 @@ public class GeneralSliderWindow : MonoBehaviour {
 	}
 
 	void UpdateDisplay () {
-		//RectTransform tmp = panel.GetComponent<RectTransform> ();
+		float canvasHeight = QuickLinks.quickLinks.mainCanvas.GetComponent<RectTransform> ().rect.height;
+		float canvasWidth = QuickLinks.quickLinks.mainCanvas.GetComponent<RectTransform> ().rect.width;
+		float directionModifier;
+		switch (edge) {
+		case ScreenEdge.Left:
+			break;
+		case ScreenEdge.Right:
+			break;
+		case ScreenEdge.Up:
+			break;
+		case ScreenEdge.Down:
+			directionModifier = -canvasHeight;
+			break;
+		default:
+			Debug.LogWarning ("SliderWindow Direction cannot be determined.");
+			break;
+		}
 		if (open) {
 			//print ("open");
 			//If toolbar is meant to be fully open but is not yet in position, continue sliding it upward by decreasing currentPercentage.
-			if (-QuickLinks.quickLinks.mainCanvas.GetComponent<RectTransform> ().rect.height * (currentPercentage - Time.deltaTime * slideSpeed) < 0) {
+			if (-canvasHeight * (currentPercentage - Time.deltaTime * slideSpeed) < openPercentage) {
 				currentPercentage -= Time.deltaTime * slideSpeed;
 			} else {
-				currentPercentage = 0;
+				currentPercentage = openPercentage;
 			}
 		} else {
 			//If meant to be closed but not yet in position, slide into position by increasing currentPercentage.
-			if (-QuickLinks.quickLinks.mainCanvas.GetComponent<RectTransform> ().rect.height * (currentPercentage + Time.deltaTime * slideSpeed) > -QuickLinks.quickLinks.mainCanvas.GetComponent<RectTransform> ().rect.height * closedPercentage + 0.1f) {
+			if (-canvasHeight * (currentPercentage + Time.deltaTime * slideSpeed) > -canvasHeight * closedPercentage + 0.1f) {
 				currentPercentage += Time.deltaTime * slideSpeed;
 			} else {
 				currentPercentage = closedPercentage;
@@ -56,7 +75,7 @@ public class GeneralSliderWindow : MonoBehaviour {
 			//print (tmp.anchoredPosition.y + ", " + -QuickLinks.quickLinks.mainCanvas.GetComponent<RectTransform> ().rect.height * closedPercentage);
 		}
 		//Update toolbar position using currentPercentage.
-		panel.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (0.0f, -QuickLinks.quickLinks.mainCanvas.GetComponent<RectTransform> ().rect.height * currentPercentage);
+		panel.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (0.0f, -canvasHeight * currentPercentage);
 	}
 
 	public void CloseWindow () {
